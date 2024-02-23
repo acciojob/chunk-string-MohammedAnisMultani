10a0eb3915c80c85e9bf58f862d9c7aa9096dd07
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-02-23 22:09:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ for(var j=i; j<size+i; j++){
     
 }
 arr.push(string);
-i=j
+i+=size;
 }
 return arr;
 }
 
 // Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size)); 
+// const str = prompt("Enter String.");
+// const size = prompt("Enter Chunk Size.");
+// alert(stringChop(str, size)); 
